45862d5855298fc0fd17ea69841e04c70a0b0bdf
amaralfatah <amaralfatah.main@gmail.com>
2024-05-10 10:35:43 +0700

    setup simulasi

--- app/src/main/java/com/uadev/cryptomind/views/SimulasiActivity.kt
+++ app/src/main/java/com/uadev/cryptomind/views/SimulasiActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -18,6 +19,9 @@ class SimulasiActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySimulasiBinding
 
+    private var currentStep = 1
+    private val totalSteps = 4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySimulasiBinding.inflate(layoutInflater)
@@ -25,13 +29,29 @@ class SimulasiActivity : AppCompatActivity() {
 
         // Aktifkan action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Program"
+        supportActionBar?.title = "Simulasi"
         supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.primary_color)))
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        updateTutorialStep(currentStep)
+
+        binding.nextButton.setOnClickListener {
+            if (currentStep < totalSteps) {
+                currentStep++
+                updateTutorialStep(currentStep)
+            }
+        }
+
+        binding.prevButton.setOnClickListener {
+            if (currentStep > 1) {
+                currentStep--
+                updateTutorialStep(currentStep)
+            }
         }
 
     }
@@ -45,6 +65,34 @@ class SimulasiActivity : AppCompatActivity() {
             }
 
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateTutorialStep(step: Int) {
+        when (step) {
+            1 -> {
+                // Mengatur gambar dan teks untuk langkah 1
+                binding.imageView.setImageResource(R.drawable.ic_simulasi)
+                binding.textView.text = getString(R.string.next)
+                binding.prevButton.visibility = View.INVISIBLE // Tombol prev tidak terlihat pada langkah pertama
+            }
+            2 -> {
+                // Mengatur gambar dan teks untuk langkah 2
+                binding.imageView.setImageResource(R.drawable.ic_kuis)
+                binding.textView.text = getString(R.string.intro2)
+                binding.prevButton.visibility = View.VISIBLE // Tombol prev terlihat pada langkah kedua
+            }
+            3 -> {
+                // Mengatur gambar dan teks untuk langkah 3
+                binding.imageView.setImageResource(R.drawable.ic_program)
+                binding.textView.text = getString(R.string.app_name)
+            }
+            4 -> {
+                // Mengatur gambar dan teks untuk langkah 4
+                binding.imageView.setImageResource(R.drawable.ic_materi)
+                binding.textView.text = getString(R.string.dekripsi)
+                binding.nextButton.text = "Finish" // Mengubah teks tombol next menjadi "Finish" pada langkah terakhir
+            }
         }
     }
 }

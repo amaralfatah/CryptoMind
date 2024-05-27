bc8048e1bcafadac43cdddde05a8cec57a3c60d6
amaralfatah <amaralfatah.main@gmail.com>
2024-05-27 14:28:03 +0700

    add materi sampai x1

--- app/src/main/java/com/uadev/cryptomind/views/materi/LatihanActivity.kt
+++ app/src/main/java/com/uadev/cryptomind/views/materi/LatihanActivity.kt
@@ -1,21 +1,45 @@
 package com.uadev.cryptomind.views.materi
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.uadev.cryptomind.R
+import com.uadev.cryptomind.databinding.ActivityLatihanBinding
+import com.uadev.cryptomind.databinding.ActivityMateriBinding
 
 class LatihanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLatihanBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_latihan)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityLatihanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aktifkan action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Latihan"
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.primary_color)))
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Handle klik tombol kembali di action bar
+                onBackPressed()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 }

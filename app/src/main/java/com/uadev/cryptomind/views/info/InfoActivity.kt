2af3d766becb2ece89de709ab8f73e21690ffdfe
amaralfatah <amaralfatah.main@gmail.com>
2024-06-05 13:44:36 +0700

    add info content

--- app/src/main/java/com/uadev/cryptomind/views/info/InfoActivity.kt
+++ app/src/main/java/com/uadev/cryptomind/views/info/InfoActivity.kt
@@ -1,21 +1,57 @@
 package com.uadev.cryptomind.views.info
 
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import com.uadev.cryptomind.R
+import com.uadev.cryptomind.databinding.ActivityInfoBinding
 
 class InfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aktifkan action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Info"
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.primary_color)))
+
+        setupDeveloperLink()
+    }
+
+    private fun setupDeveloperLink() {
+        val developerText = binding.tvDev.text.toString()
+        val developerSpannableString = SpannableString(developerText)
+
+        val githubSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/amaralfatah/CryptoMind"))
+                startActivity(intent)
+            }
         }
+
+        val developerStart = developerText.indexOf("Amar Al Fatah")
+        val developerEnd = developerStart + "Amar Al Fatah".length
+        developerSpannableString.setSpan(
+            githubSpan,
+            developerStart,
+            developerEnd,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.tvDev.text = developerSpannableString
+        binding.tvDev.movementMethod = LinkMovementMethod.getInstance()
     }
 }
